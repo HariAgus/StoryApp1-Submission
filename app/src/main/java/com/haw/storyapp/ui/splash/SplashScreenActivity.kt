feffdfa419e9fa3ca18822e1b573e75ab23804da
HariAgus <haryagus97@gmail.com>
2022-05-26 00:57:51 +0700

    Create login screen and item list story

--- app/src/main/java/com/haw/storyapp/ui/splash/SplashScreenActivity.kt
+++ app/src/main/java/com/haw/storyapp/ui/splash/SplashScreenActivity.kt
@@ -3,8 +3,8 @@ package com.haw.storyapp.ui.splash
 import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.haw.storyapp.MainActivity
 import com.haw.storyapp.databinding.ActivitySplashScreenBinding
+import com.haw.storyapp.ui.login.LoginActivity
 import com.haw.storyapp.utils.startActivity
 
 @SuppressLint("CustomSplashScreen")
@@ -21,7 +21,7 @@ class SplashScreenActivity : AppCompatActivity() {
             alpha = 0F
             animate().setDuration(2000).alpha(1F).withEndAction {
                 binding.tvNameApp.alpha = 1F
-                startActivity<MainActivity>()
+                startActivity<LoginActivity>()
                 overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
                 finish()
             }
